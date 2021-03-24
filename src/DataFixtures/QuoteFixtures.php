e885cd56977de15eadee4b49486cb9afbfe3004b
Antoine07 <antoine.lucsko@gmail.com>
2021-03-24 11:33:12 +0100

    correction exercice 1

--- src/DataFixtures/QuoteFixtures.php
+++ src/DataFixtures/QuoteFixtures.php
@@ -8,26 +8,29 @@ use Faker;
 
 class QuoteFixtures extends Fixture
 {
+
+    const PRIORITY_NONE = 'none';
+    const PRIORITY_IMPORTANT = 'important';
+
     public function load(ObjectManager $manager)
     {
         $faker =  Faker\Factory::create('fr_FR');
         $faker->addProvider(new \DavidBadura\FakerMarkdownGenerator\FakerProvider($faker));
 
         for ($i = 0; $i < 10; $i++) {
-            $post = new Quote;
-            $post->setTitle($faker->catchPhrase)
+            $quote = new Quote;
+            $quote->setPosition( random_int(1, 2) === 1 ?  self::PRIORITY_NONE : self::PRIORITY_IMPORTANT );
+            $quote->setTitle($faker->catchPhrase)
                 ->setContent($faker->markdown);
 
-            $manager->persist($post);
+            $date = new \DateTime('2021-01-01 00:00:00');
+            $seconds = random_int(24*60, 100*24*60);
+            $date->add(new \DateInterval("PT{$seconds}S"));
+            $quote->setCreatedAt($date);
+
+            $manager->persist($quote);
         }
 
         $manager->flush();
     }
-
-    // public function getDependencies()
-    // {
-    //     return array(
-    //         AppFixtures::class,
-    //     );
-    // }
 }
